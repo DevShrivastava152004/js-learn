d6dea253b68371f847ad2da98490d86b8b775f66
Dev Shrivastava <165247828+DevShrivastava152004@users.noreply.github.com>
2025-07-28 11:21:55 +0000

    initial commit

---
+++ 01-basics/datatypes-summary.js
@@ -0,0 +1,30 @@
+//primitive
+
+//7 types: string, Number, Boolearn, null , undefined, Symbol ,BigInt
+
+// Reference (Non primitive)
+
+//Array, Objects, Functions
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+const bigNumber = 34567473737483n
+// const heros ["shaktiman", "naagraj", "doga" ]
+// let myObj={
+//     name: "hitesh",
+//     age: 22,
+// }
+// const myFunction = function(){
+//     console.log("hello world");
+// }
+console.log(typeof bigNumber);
+
+
