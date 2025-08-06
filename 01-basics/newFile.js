d8c4c342cae2107a8ce75e22bbfb111eb4f3ebcf
Dev Shrivastava <165247828+DevShrivastava152004@users.noreply.github.com>
2025-08-06 03:43:56 +0000

    strings

---
+++ 01-basics/newFile.js
@@ -0,0 +1,11 @@
+const { repoCount } = require("./05_strings");
+
+console.log(`hello m name is ${name} and my repo count is ${repoCount}');
+    
+const gameName = new String('hiteshhc')
+
+// console.log(gameName[0]);
+// console.log(gameName.tOUpperCase());
+console.log(gameName.charAt(2));
+
+`);
