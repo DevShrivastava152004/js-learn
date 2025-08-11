6273770e0a49f43664eb481565d100402d582493
Dev Shrivastava <165247828+DevShrivastava152004@users.noreply.github.com>
2025-08-11 14:39:59 +0000

    initial commit

--- 02-basics/04_objects.js
+++ 02-basics/04_objects.js
@@ -1,4 +1,4 @@
-// const tinderUser = new Object()
+//  const tinderUser = new Object()
 const tinderUser = {}
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
@@ -22,5 +22,23 @@ const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
 // const obj3 = { obj1, obj2}
-const obj3 = Object.assign({},obj1, obj2, obj4)
-console.log(obj3); 
+// const obj3 = Object.assign({},obj1, obj2, obj4)
+// console.log(obj3); 
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    }
+]
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));//output value ka dataype hai array
+console.log(Object.values(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))
